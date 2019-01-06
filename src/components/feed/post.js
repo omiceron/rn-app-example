727da45b05f768f254023c31e61e0be1668344b9
Constantine Sharow <omiceron@gmail.com>
2019-01-06 20:34:54 +0500

    Post restyled

--- src/components/feed/post.js
+++ src/components/feed/post.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react'
-import {View, StyleSheet, Text} from 'react-native'
+import {SafeAreaView, StyleSheet, Text, ScrollView} from 'react-native'
+import {MapView, Permissions, Location} from 'expo'
 import PropTypes from 'prop-types'
 import AttachedMap from './attached-map'
+import TableView from '../common/table-view'
+import TableRow from '../common/table-row'
+import {REGION_DELTAS} from '../../constants'
 
 class Post extends Component {
   static propTypes = {
@@ -15,20 +19,51 @@ class Post extends Component {
   render() {
     const {title, text, coords} = this.props.post
 
-    return <View>
-      <Text>
-        {title}
-      </Text>
-      <Text>
-        {text}
-      </Text>
-      {coords && <AttachedMap onPress = {() => alert('map pressed')} coords = {coords}/>}
-    </View>
+    const renderMap = () => {
+      if (!coords) return null
+
+      return <MapView
+        style = {styles.mapView}
+        initialRegion = {{...coords, ...REGION_DELTAS}}
+      >
+        <MapView.Marker
+          coordinate = {{...coords}}
+        />
+      </MapView>
+
+    }
+
+    return <TableView scrollable>
+        <TableRow disableSeparator>
+          <Text style = {styles.title}>
+            {title}
+          </Text>
+        </TableRow>
+        <TableRow>
+          <Text style = {styles.text}>
+            {text}
+          </Text>
+        </TableRow>
+      {renderMap()}
+    </TableView>
   }
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '100'
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '100'
+  },
+  mapView: {
+    height: 100
+  },
 })
 
 export default Post
