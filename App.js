12fe2d40d60b75da5055a369403cdec7f59b253c
Constantine Sharow <omiceron@gmail.com>
2019-02-20 23:36:32 +0500

    Update App Loading flow

--- App.js
+++ App.js
@@ -1,22 +1,38 @@
-import {configure} from 'mobx'
-import {Provider} from 'mobx-react'
+import {configure, observable, action} from 'mobx'
+import {Provider, observer} from 'mobx-react'
 import React from 'react'
-import {StatusBar} from 'react-native'
-import {ScreenOrientation} from 'expo'
+import {StatusBar, AsyncStorage} from 'react-native'
+import {ScreenOrientation, AppLoading, Font, Asset} from 'expo'
 import {config} from './src/config'
 import AppNavigator from './src/components/app-navigator'
 import stores from './src/stores'
 
 configure({enforceActions: 'never'})
 
+@observer
 export default class App extends React.Component {
 
-  componentWillMount() {
+  @observable isReady = false
+  @action setReady = () => this.isReady = true
+
+  @action _loadAssetsAsync = async () => {
     StatusBar.setBarStyle('light-content', true)
-    ScreenOrientation.allowAsync(Expo.ScreenOrientation.Orientation.PORTRAIT_UP)
+    await ScreenOrientation.allowAsync(Expo.ScreenOrientation.Orientation.PORTRAIT_UP)
+    await Font.loadAsync({
+      'Meowchat': require('./assets/fonts/meowchat-icons.ttf')
+    })
+
+    await Asset.fromModule(require('./assets/images/splash.png')).downloadAsync()
   }
 
   render() {
+    if (!this.isReady) return <AppLoading
+      startAsync = {this._loadAssetsAsync}
+      onFinish = {this.setReady}
+      onError = {console.warn}
+      autoHideSplash = {false}
+    />
+
     return <Provider {...stores}>
       <AppNavigator ref = {this.setNavRef}/>
     </Provider>
