25433d51be63e16442659f1229ebddc8535f8fe0
Constantine Sharow <omiceron@gmail.com>
2019-02-23 22:27:36 +0500

    Update Basic Store

--- src/stores/basic-store.js
+++ src/stores/basic-store.js
@@ -1,9 +1,19 @@
 export default class BasicStore {
-    constructor(stores) {
-      this._stores = stores
-    }
+  constructor(stores, storeName) {
+    this._stores = stores
+    this._storeName = storeName
+  }
 
-    getStore(storeName) {
-        return this._stores[storeName]
-    }
+  getStore(storeName) {
+    return this._stores[storeName]
+  }
+
+  get storeName() {
+    return this._storeName
+  }
+
+  get constructorName () {
+    return this.__proto__.constructor.name
+  }
+
 }
