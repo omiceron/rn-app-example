e3886577e12c942c96565626cc8ea56be364cafc
Constantine Sharow <omiceron@gmail.com>
2019-03-10 23:58:44 +0500

    Update Chat Components

--- src/components/messenger/message.js
+++ src/components/messenger/message.js
@@ -9,6 +9,7 @@ import {
 } from '../../constants'
 import {observer, inject} from 'mobx-react'
 import Icon from 'react-native-vector-icons/Ionicons'
+import MessageAttachments from './message-attachments'
 
 @inject(AUTH_STORE)
 class Message extends PureComponent {
@@ -17,51 +18,35 @@ class Message extends PureComponent {
     text: string.isRequired,
     timestamp: number.isRequired,
     key: string,
-    pending: bool
+    pending: bool,
+    attachments: array
   }
 
-  renderStatus = () => <View style = {{
-      justifyContent: 'center',
-      marginLeft: 8,
-      marginTop: 2,
-    }}>
-      <Icon
-        name = {`ios-${this.props.pending ? 'checkmark' : 'done-all'}`}
-        color = {INACTIVE_TEXT_COLOR}
-        size = {30}/>
-    </View>
+  renderStatus = () => <View style = {styles.status}>
+    <Icon
+      name = {`ios-${this.props.pending ? 'checkmark' : 'done-all'}`}
+      color = {INACTIVE_TEXT_COLOR}
+      size = {30}/>
+  </View>
 
   render() {
-    const {userId, text, timestamp, pending} = this.props
+    const {userId, text, timestamp, pending, attachments} = this.props
     const isCurrentUser = this.props.auth.user.uid === userId
-    // const date = new Date(timestamp).toTimeString().slice(0, 5)
 
-    /*
-        const renderDate = () => <View>
-          <Text selectable style = {[{color: isCurrentUser ? '#E9E9E9' : 'grey'}, styles.text]}>
-            {date}
+    return <View style = {[styles.container, {alignSelf: isCurrentUser ? 'flex-end' : 'flex-start'}]}>
+      {isCurrentUser && this.renderStatus()}
+      <View style = {[styles.messageContainer, {backgroundColor: isCurrentUser ? USER_MESSAGE_COLOR : MESSAGE_COLOR}]}>
+
+        <View style = {styles.textContainer}>
+          <Text selectable style = {[styles.text, {color: isCurrentUser ? WHITE_BACKGROUND_COLOR : BLACK_TEXT_COLOR}]}>
+            {text}
           </Text>
         </View>
-    */
 
-    return <View style = {{
-      flex: 1,
-      alignSelf: isCurrentUser ? 'flex-end' : 'flex-start',
-      flexDirection: 'row'
-    }}>
-      {/*{pending && <ActivityIndicator/>}*/}
-      {isCurrentUser && this.renderStatus()}
-      <View
-        // onLayout = {({nativeEvent: {layout: {height}}}) => this.props.message.height = height}
-        style = {[styles.container, {
-          backgroundColor: isCurrentUser ? USER_MESSAGE_COLOR : MESSAGE_COLOR
-        }]}>
-        <Text selectable style = {[styles.text, {
-          maxWidth: WINDOW_WIDTH - WINDOW_WIDTH / 5,
-          color: isCurrentUser ? WHITE_BACKGROUND_COLOR : BLACK_TEXT_COLOR
-        }]}>
-          {text}
-        </Text>
+        <View style = {styles.textContainer}>
+          {attachments && attachments.length ?
+            <MessageAttachments style = {styles.textContainer} attachments = {attachments}/> : null}
+        </View>
       </View>
     </View>
   }
@@ -70,19 +55,32 @@ class Message extends PureComponent {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  messageContainer: {
     borderRadius: 12,
     marginLeft: 8,
     marginRight: 8,
     marginTop: 2,
     marginBottom: 2,
     display: 'flex',
-    flexDirection: 'row',
     paddingHorizontal: 12,
     paddingVertical: 8
+  },
+  textContainer: {
+    maxWidth: WINDOW_WIDTH - WINDOW_WIDTH / 5,
+    alignItems: 'flex-end'
   },
   text: {
     fontSize: 16,
     fontWeight: '100'
+  },
+  status: {
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    marginLeft: 8,
+    marginTop: 2
   }
 })
 
