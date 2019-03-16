80a664ecfefa2030da8179f0691ef19ac57af6df
Constantine Sharow <omiceron@gmail.com>
2019-03-16 14:06:01 +0500

    Update PostForm

--- src/components/feed/post-form.js
+++ src/components/feed/post-form.js
@@ -1,20 +1,23 @@
 import React, {Component} from 'react'
-import {View, StyleSheet, TextInput, SafeAreaView, LayoutAnimation, Text} from 'react-native'
+import {reaction} from 'mobx'
+import {observer, inject} from 'mobx-react'
+import {View, StyleSheet, TextInput, SafeAreaView, LayoutAnimation} from 'react-native'
+import {isIphoneX, getBottomSpace} from 'react-native-iphone-x-helper'
 import {bool, string, func, shape, objectOf, number} from 'prop-types'
+
+import {KEYBOARD, FEED_STORE, BLACK_TEXT_COLOR, WHITE_BACKGROUND_COLOR, NAVIGATION_STORE} from '../../constants/'
+
+import withAnimation from '../common/with-animation'
+import withAttachments from '../common/with-attachments'
+
 import TableView from '../common/table-view'
 import TableRow from '../common/table-row'
-import withAnimation from '../common/with-animation'
-import {KEYBOARD, FEED_STORE} from '../../constants/'
-import {observer, inject} from 'mobx-react'
-import Icon from 'react-native-vector-icons/Ionicons'
-import {NAVIGATION_STORE} from '../../constants/index'
-import AttachedMap from './attached-map'
-import {isIphoneX, getBottomSpace} from 'react-native-iphone-x-helper'
-import {reaction} from 'mobx'
-import {BLACK_TEXT_COLOR, INACTIVE_TEXT_COLOR, WHITE_BACKGROUND_COLOR} from '../../constants'
+import AttachmentsList from '../messenger/attachments-list'
+import AttachedLocation from './attached-location'
+import PostFormControlRow from './post-form-control-row'
 
-// navigation
-
+@withAttachments()
+@withAnimation(KEYBOARD)
 @inject(NAVIGATION_STORE)
 @inject(FEED_STORE)
 @observer
@@ -27,10 +30,16 @@ class PostForm extends Component {
       () => this.props.layouts[KEYBOARD],
       () => LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
     )
+
+    reaction(
+      () => this.props.attachmentsList,
+      () => this.props.getAttachmentsHelper(this.props.attachmentsList)
+    )
   }
 
   static propTypes = {
-    layouts: objectOf(objectOf(number)).isRequired,
+    // layouts: objectOf(objectOf(number)).isRequired,
+    getAttachmentsHelper: func.isRequired,
     feed: shape({
       coords: shape({
         latitude: number.isRequired,
@@ -45,53 +54,53 @@ class PostForm extends Component {
       setTitle: func.isRequired,
       setText: func.isRequired,
       clearPostForm: func.isRequired
-    }).isRequired
+    })
   }
 
   componentWillUnmount() {
     this.props.feed.clearPostForm()
   }
 
-  renderIcon = () => <Icon color = {'#C7C7CD'} size = {16} name = 'ios-pin'/>
-
   render() {
-    const {text, title, setText, setTitle, attachedCoords} = this.props.feed
+    const {feed} = this.props
     const {height} = this.props.layouts[KEYBOARD]
+
     return <SafeAreaView style = {styles.container}>
       <TableView style = {styles.container}>
 
         <TableRow>
-          <TextInput autoFocus
-                     style = {styles.text}
-                     placeholder = 'Enter title here...'
-                     returnKeyType = 'next'
-                     value = {title}
-                     onChangeText = {setTitle}
-                     onSubmitEditing = {() => this.textInput.focus()}/>
+          <TextInput
+            autoFocus
+            style = {styles.text}
+            placeholder = 'Enter title here...'
+            returnKeyType = 'next'
+            value = {feed.title}
+            onChangeText = {feed.setTitle}
+            onSubmitEditing = {() => this.textInput.focus()}
+          />
         </TableRow>
 
         <TableRow style = {styles.textRow}>
-          <TextInput value = {text}
-                     onChangeText = {setText}
-                     ref = {ref => this.textInput = ref}
-                     style = {styles.text}
-                     placeholder = 'Enter text here...'
-                     multiline/>
+          <TextInput
+            value = {feed.text}
+            onChangeText = {feed.setText}
+            ref = {ref => this.textInput = ref}
+            style = {styles.text}
+            placeholder = 'Enter text here...'
+            multiline
+          />
         </TableRow>
 
-        <TableRow
-          RightComponent = {attachedCoords ? null : this.renderIcon}
-          onPress = {() => this.props.navigation.navigate('locationForm')}
-          disableSeparator
-        >
-          {attachedCoords ? <AttachedMap onPress = {() => this.props.navigation.navigate('locationForm')}
-                                         coords = {attachedCoords}/> :
-            <Text style = {[styles.text, {color: '#C7C7CD'}]}>
-              Add location
-            </Text>
+        {this.props.attachmentsList.length ? <AttachmentsList attachments = {this.props.attachmentsList}/> : null}
 
-          }
-        </TableRow>
+        {feed.attachedLocation ? <TableRow>
+          <AttachedLocation disableIcon location = {feed.attachedLocation}/>
+        </TableRow> : null}
+
+        <PostFormControlRow
+          attachImageHandler = {this.props.attachImageHandler}
+          attachPhotoHandler = {this.props.attachPhotoHandler}
+        />
 
         <View style = {{height: height - (isIphoneX() && getBottomSpace())}}/>
 
@@ -114,9 +123,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: '100',
     color: BLACK_TEXT_COLOR
-  }
+  },
 })
 
-export default withAnimation(PostForm, {
-  layoutNames: [KEYBOARD]
-})
+export default PostForm
--- src/components/screens/post-form-screen.js
+++ src/components/screens/post-form-screen.js
@@ -7,23 +7,34 @@ import NavigationButton from '../navigation/navigation-button'
 @inject(FEED_STORE)
 @observer
 class PostFormScreen extends Component {
+  constructor(...args) {
+    super(...args)
+
+    const {sendPost} = this.props.feed
+    const getAttachments = () => this.attachments
+    this.props.navigation.setParams({sendPost, getAttachments})
+
+  }
+
   static navigationOptions = ({navigation}) => {
     const sendPost = navigation.getParam('sendPost')
+    const getAttachments = navigation.getParam('getAttachments')
 
     return ({
       title: 'Add post',
       headerLeft: <NavigationButton title = 'Cancel' onPress = {() => navigation.navigate('feed')}/>,
-      headerRight: <NavigationButton title = 'Send' onPress = {sendPost}/>
+      headerRight: <NavigationButton title = 'Send' onPress = {() => sendPost(getAttachments())}/>
     })
   }
 
-  componentWillMount() {
-    const {sendPost} = this.props.feed
-    this.props.navigation.setParams({sendPost})
+  getAttachmentsHelper = (attachments) => {
+    this.attachments = attachments
   }
 
   render() {
-    return <PostForm/>
+    return <PostForm
+      getAttachmentsHelper = {this.getAttachmentsHelper}
+    />
   }
 
 }
